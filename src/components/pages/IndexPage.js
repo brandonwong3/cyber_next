cc92a0428cf3bc75c7e1f1e4f569cd1538b30fe9
bandhna <bandhna@uw.edu>
2023-02-27 16:53:02 -0800

    created 3 column boxes that will link to pages

--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -6,11 +6,13 @@ const IndexPage = () => {
 	return (
 		<BaseTemplate>
 			<div className={"mx-auto flex justify-center flex-grow flex-1 align-center"}>
+				<div className='homeAbout'>
 				<h1>Welcome to PenSec!</h1>
 				<p>An educational and informative website for small businesses and students
 					to learn more about penetration testing. With easy to follow guided demos and
 					resources on the different areas of pen testing to better secure your business, PenSec
 					has guidance for everyone!</p>
+					</div>
 			</div>
 			<div className="teaser">
 				<div className="contents">
@@ -26,7 +28,7 @@ const IndexPage = () => {
 						</div>
 						<div className="box-container">
 							<div className="box">
-							<img className='icon' src='https://d33wubrfki0l68.cloudfront.net/f5f8ace2056c06f0467ee81a47ed8546a1f2c308/bb881/static/penetration_testing_secora-7844f476d65cdd635467a6fc43fcef01.png' alt='computer with bug magnified' />
+							<img className='icon' src='https://d33wubrfki0l68.cloudfront.net/f5f8ace2056c06f0467ee81a47ed8546a1f2c308/bb881/static/penetration_testing_secora-7844f476d65cdd635467a6fc43fcef01.png' alt='lock in a box with lines coming out.' />
 								</div>
 							<p className="caption">What Pen Testing Is</p>
 						</div>
