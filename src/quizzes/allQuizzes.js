b948a638104f0572610cb6638431084298bce8bb
brandonwong3 <bwong3@uw.edu>
2023-03-02 14:52:28 -0800

    Fixing Navbar and PracticePage."

--- src/quizzes/allQuizzes.js
+++ src/quizzes/allQuizzes.js
@@ -1,14 +1,14 @@
 import AUTHENTICATION_QUIZ from './authenticationQuiz';
 import SQL_INJECTION_QUIZ from './sqlIQuiz';
 import XSS_QUIZ from './xssQuiz';
-import TEST_QUIZ from './testQuiz';
+// import TEST_QUIZ from './testQuiz';
 
 
 const ALL_QUIZZES = [
 	AUTHENTICATION_QUIZ,
 	SQL_INJECTION_QUIZ,
 	XSS_QUIZ,
-	TEST_QUIZ
+	// TEST_QUIZ
 ];
 
 const allQuizUrlPaths = ALL_QUIZZES.map(quiz => quiz.urlPath);
