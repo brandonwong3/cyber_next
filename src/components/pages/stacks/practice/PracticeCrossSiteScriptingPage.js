5970b78c6fe87b0365df096f3c387787771006c2
isaxena02 <ishita.saxena1103@gmail.com>
2023-03-01 18:30:14 -0800

    clean up code

--- src/components/pages/stacks/practice/PracticeCrossSiteScriptingPage.js
+++ src/components/pages/stacks/practice/PracticeCrossSiteScriptingPage.js
@@ -1,12 +1,36 @@
 import React from 'react';
-
+import BaseTemplate from '../../../templates/BaseTemplate';
+import { Link } from 'react-router-dom';
 
 const PracticeCrossSiteScriptingPage = () => {
 	return (
+		<BaseTemplate>
 		<div className={"mx-auto flex justify-center flex-grow flex-1 align-center"}>
-			<h1>Practice Cross Site Scripting Page</h1>
-			<p>Hello world</p>
+			<h1>Example Cross-Site Scripting</h1>
 		</div>
+		<div className ="text-container">
+			<p> Given a website with the following <strong>URL</strong>: https://website.com/status?message=Hello+World.</p>
+			<p> It displays this on the page: </p>
+		</div>
+		<div className ="image-container">
+			<img src="/../../../../images/XSS-link.png" alt="text display"></img>
+		</div>
+		<div className = "text-container">
+			<p>Note how user input is directly put on the page with no sanitation, indicating a potential reflected cross-site scripting vulnerability. </p>
+			<p><strong>An attacker can create a malicious link due to this lack of data processing:</strong></p>
+		</div>
+		<div className = "image-container">
+			<img src="/../../../../images/bad-link.png" alt="example of malicious link"></img>
+		</div>
+		<div className ="text-container">
+			<p> An attacker can add an alert(1) within the script tags and have a pop-up come up every time this link is visited and control the user’s session with the application.</p>
+		</div>
+		<div className="btn-container">
+ 				<Link to= "/practice/cross-site-scripting">
+					 <button>Try It!</button>
+				 </Link>
+			</div>
+		</BaseTemplate>
 	);
 }
 
--- src/components/pages/stacks/practice/PracticeSQLInjectionPage.js
+++ src/components/pages/stacks/practice/PracticeSQLInjectionPage.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import BaseTemplate from '../../../templates/BaseTemplate';
+import { Link } from 'react-router-dom';
 
-
-const PracticeSQLInjectionPage = () => {
+const PracticeSQLInjectionPage= () => {
 	return (
+		<BaseTemplate>
 		<div className={"mx-auto flex justify-center flex-grow flex-1 align-center"}>
-			<h1>Practice SQL Injection Page</h1>
-			<p>Hello world</p>
+			<h1>Example SQL Injection</h1>
 		</div>
+		<div className ="text-container">
+			<p> Consider an application that sells products from various different categories. When a user selects a category, such as Clothing, the browser requests the following <strong>URL</strong>:</p>
+			<p>https://shopping-website.com/products?category=Clothing</p>
+			<p><em><strong>In the Database, this request is seen as a SQL query request to retrieve details related to Clothing:</strong></em></p>
+			<p>SELECT * FROM products WHERE category = ‘Clothing’ AND inStock = 1 </p>
+			<p><strong>This query asked the database to return:</strong></p>
+			<li><u>all details</u> (*) from the <u>products</u> table where the <u>category</u> is Clothing and inStock is 1.</li>
+				<li>the <u>inStock=1</u> restriction hides products that aren’t inStock</li>
+			<p><strong>The application doesn’t have any defense mechanisms so an attacker can create a malicious payload:</strong></p>
+			<p>https://shopping-website.com/products?category=Clothing’--</p>
+			<p><strong>This results in the query:</strong></p>
+			<p>SELECT * FROM products WHERE category = ‘Clothing’--' AND inStock = 1</p>
+			<li>because -- is a comment in SQL, so it removes the inStock=1 restriction in the query.</li>
+			<p>An attacker can also return information they don’t know about like so:</p>
+			<p>https://shopping-website.com/products?category=Clothing’+OR+1=1--</p>
+			<p><strong>This results in the query:</strong></p>
+			<p>SELECT * FROM products WHERE category = ‘Clothing’ OR 1=1--' AND inStock = 1</p>
+			<li>because 1=1 is always true, all items will be returned by the query.</li>
+		</div>
+		<div className="btn-container">
+ 				<Link to= "/practice/sql-injection-scripting">
+					 <button>Try It!</button>
+				 </Link>
+			</div>
+		</BaseTemplate>
 	);
 }
 
