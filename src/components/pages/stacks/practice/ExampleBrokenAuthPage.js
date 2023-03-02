7e30a6a7dc83b781cb1541b24816340580c0bff0
isaxena02 <ishita.saxena1103@gmail.com>
2023-03-01 16:35:00 -0800

    some code for broken auth reading and example

---
+++ src/components/pages/stacks/practice/ExampleBrokenAuthPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import BaseTemplate from '../../../templates/BaseTemplate';
+import { Link } from 'react-router-dom';
+
+const ExampleBrokenAuthPage = () => {
+	return (
+		<BaseTemplate>
+		<div className={"mx-auto flex justify-center flex-grow flex-1 align-center"}>
+			<h1>Example Broken Auth Page</h1>
+		</div>
+		<div className ="text-container">
+			<p> Someone may want to test login security by attempting to bypass authentication (username, password login) by exploiting a cookie. </p>
+			<p> Using an application security software tester like <a href="https://portswigger.net/burp">Burp Suite</a> can help us search for exploitable vulnerabilities. </p>
+			<p> Intercept the Request with proxy tools such as Burp suite and analyze the backend details. While intercepting the request, one will see something like this: </p>
+		</div>
+		<div className="img-container">
+			<img src="/../../../../images/brokenauth.jpg" alt="Intercepting request"></img>
+		</div>
+		<div className ="text-container">
+			<p>Since the Cookie “UserId” has been sent to us by the server so it can be modified to check the profiles of other users by manipulating the cookie. We will try to brute force the USERID cookie and will check for the response.</p>
+		</div>
+		<div className="img-container">
+			<img src="/../../../../images/response.png" alt="Cookie User ID Response"></img>
+		</div>
+		<div className="text-container">
+			<p>After the Bruteforcing the USERID cookie, we will see the response, which will be showing OK (200) code, it means that this particular combination work for the user id.</p>
+		</div>
+		<div className="img-container">
+			<img src= "../../../../../images/ok-response.png" alt="OK Response Code">
+			</img>
+		</div>
+		<div className ="text-container">
+			<p>We can see that there are so many requests that all are having OK status, and when we clicked on Request no 441, we saw that the user id brute force was 10411. Its username is AndyPaul, and its default password was “PASSWORD.” Hence in this way, we can extract an ample number of User Accounts if the Broken Authentication Vulnerability exists in the Web Application.</p>
+		</div>
+		<div className="btn-container">
+ 				<Link to= "/practice/broken-auth">
+					 <button>Try It!</button>
+				 </Link>
+			</div>
+		</BaseTemplate>
+	);
+}
+
+export default ExampleBrokenAuthPage;
---
+++ src/components/pages/stacks/practice/ReadingBrokenAuthPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BaseTemplate from '../../../templates/BaseTemplate';
+
+
+// Need to work on Drop Downs
+const ReadingBrokenAuthPage = () => {
+	return (
+		<BaseTemplate>
+		<div className={"mx-auto flex justify-center flex-grow flex-1 align-center"}>
+			<div className="page-title">
+				<h1>Broken Authentication</h1>
+			</div>
+			</div>
+			<div className="broken-auth-desc">
+				<h2>What is Broken Authentication?</h2>
+				<p>
+				Broken authentication is a term that refers to vulnerabilities involving attackers impersonating users. Broken authentication refers to weaknesses in two areas: <em>session management</em> and <em>credential management</em>.</p>
+				<p>
+					<h3>Types of Broken Authentication Attacks:</h3>
+					<li><strong>Credential stuffing</strong>- A hacker uses stolen usernames and passwords from one web application to access user accounts at other web applications.</li>
+					<li><strong> Session Hijacking</strong>- Hackers using stolen session IDs to gain unauthorized access to a system and impersonate the user. These kinds of attacks can occur through vectors such as phishing attacks, public wi-fi sniffing, or malware. </li>
+					<li><strong> Password Spraying</strong>- A type of brute force attack (an attacker submitting many passwords or passphrases with the hope of eventually guessing correctly). Attackers use lists of the most common and weak passwords. This method often pays off considering how often “password”, “123456”, and common strings that are likely used as passwords. </li>
+				</p>
+			</div>
+			<div className="risk-desc">
+				<h2>Risk Analysis:</h2>
+				<p>Risk levels associated with Broken Authentications attack can range from moderate to critical, depending on both the motives of the attacker and information/credentials linked to the attack. Occurrences of broken authentication attacks have led to: stealing critical business data, selling identity theft, personal user data, sending fraudulent calls or emails, creating malicious software programs for disrupting networks, cyberterrorism, and cyberstalking.</p>
+			</div>
+			<div className="prevention-desc">
+				<h2>How to Prevent Broken Authentication:</h2>
+				<p>
+					<li> Set strong, unique passwords (try not to use the same password for multiple web apps) </li>
+					<li> Multi-factor authentication </li>
+					<li> Enable passkeys, a new category of digital credentials for users to login without the use of complex passwords </li>
+					<li> Avoid showing Session IDs in URLs </li>
+					<li> Be mindful of session length (log out of web applications to minimize vulnerability of sessions hijacking)</li>
+				</p>
+			</div>
+			<div className="btn-container">
+ 				<Link to= "/practice/ExampleBrokenAuth">
+					 <button>Practice It!</button>
+				 </Link>
+			</div>
+		</BaseTemplate>
+	);
+}
+
+export default ReadingBrokenAuthPage;
