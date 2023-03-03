939038dcee1da9581432064a377c05a79f384a07
tyrellgarza <otgarza@uw.edu>
2023-03-02 16:20:02 -0800

    resources added

--- src/components/pages/stacks/read/ReadWhatsNextPage.js
+++ src/components/pages/stacks/read/ReadWhatsNextPage.js
@@ -15,20 +15,44 @@ const ReadWhatsNextPage = () => {
 			<div className="read-whats-next-resources-dropdowns">
 			  <div className="read-whats-next-dropdown">
 				<h3>For Students</h3>
+				<h5>Interview Prep</h5>
 				<ul>
 				  <li>
-					<a href="#">
+					<a href="https://www.simplilearn.com/tutorials/cyber-security-tutorial/cyber-security-interview-questions">
 					  Top 80+ Cybersecurity Interview Questions and Answers for 2023
 					</a>
 				  </li>
+				</ul>
+				<h5>Top Certifications</h5>
+				<ul>
+					<li>
+					<a href="https://www.infosecinstitute.com/skills/roles/penetration-tester/?utm_medium=PPC&utm_campaign=Google%20AdWords%20Branded&utm_source=GoogleAds&utm_keyword=&utm_content=554890325926&utm_term=&matchtype=&network=g&device=c&gclid=CjwKCAiArY2fBhB9EiwAWqHK6o1rYWDWxZ8ya85fZ9e7UAtH02ak9CoVcfRJJvvv04_WQV9cLHYKRBoC6t8QAvD_BwE">
+					Infosec Institute Certified Penetration Tester (CPT)
+					</a>
+					</li>
+					<li>
+					<a href="https://www.giac.org/certifications/penetration-tester-gpen/">
+					Global Information Assurance Certification (GIAC) Penetration Tester (GPEN)
+					</a>
+					</li>
+					<li>
+					<a href="https://www.offensive-security.com/courses/pen-200/?utm_campaign=Google-Ads_Brand_PPC_PWK_2020_Update_NAM&utm_medium=cpc&utm_source=google&utm_content=crid=593937234388&utm_term=kwd=oscp:cid-9248778671:kwd-314572348942:dev-c:mt-e&gclid=CjwKCAiA85efBhBbEiwAD7oLQGQBJ1PBNOM2RY_wROqoUb_0mTIxUIVDHpcrE2kXausS_C-IgiOiahoCx_oQAvD_BwE">
+					Offensive Security OSCP Certification
+					</a>
+					</li>
 				</ul>
 			  </div>
 			  <div className="read-whats-next-dropdown">
 				<h3>For Business</h3>
 				<ul>
 				  <li>
-					<a href="#">
-					  Learn about Cyber Liability Insurance, here
+					<a href="https://www.forbes.com/advisor/business-insurance/cyber-liability-insurance/">
+					  Learn about Cyber Liability Insurance.
+					</a>
+				  </li>
+				  <li>
+					<a href="https://www.ftc.gov/business-guidance/blog/2019/12/free-vulnerability-scanning-your-business">
+					Get a free vulnerability scan for your business.
 					</a>
 				  </li>
 				</ul>
