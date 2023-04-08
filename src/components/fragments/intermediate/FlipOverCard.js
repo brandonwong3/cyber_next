0d0761b19abbf20ccef882c6d697551c4331a650
brandonwong3 <bwong3@uw.edu>
2023-04-08 16:57:04 -0700

    Got the basic flip card working.

---
+++ src/components/fragments/intermediate/FlipOverCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactCardFlip from 'react-card-flip';
+
+
+const FlipOverCard = ({question, children}) => {
+
+	const [showQuestion, setShowQuestion] = React.useState(true);
+
+	return (
+		<ReactCardFlip containerClassName={"flip-over-card"} isFlipped={!showQuestion} className={`hover-pointer rounded-5 p-5 m-5 flex flex-col justify-center items-center ${showQuestion ? 'flip-over-card-front' : 'flip-over-card-back'}`} onClick={() => setShowQuestion(!showQuestion)}>
+			<div key="front" onClick={() => setShowQuestion(!showQuestion)}>
+				<h1 className={"text-center font-bold text-lg-center"}>{question}</h1>
+			</div>
+			<div key="back" onClick={() => setShowQuestion(!showQuestion)}>
+				{children}
+			</div>
+		</ReactCardFlip>
+	)
+
+}
+
+export default FlipOverCard
--- src/components/pages/TestPage.js
+++ src/components/pages/TestPage.js
@@ -2,13 +2,17 @@ import React from "react";
 import BaseTemplate from '../templates/BaseTemplate';
 import QuizPage from './QuizPage';
 import AUTHENTICATION_QUIZ from '../../quizzes/authenticationQuiz';
+import FlipOverCard from '../fragments/intermediate/FlipOverCard';
 
 
 const TestPage = () => {
 	return (
 		<BaseTemplate>
 			<div className={'force-absolute-center'}>
-				<QuizPage title={'Test Quiz Page'} questions={AUTHENTICATION_QUIZ.questions} />
+				<FlipOverCard question={'What is the difference between a cookie and a session?'}>
+					<p className={'text-center'}>A cookie is stored on the client's computer and is sent to the server with every request. A session is stored on the server and is identified by a session ID stored in a cookie.</p>
+				</FlipOverCard>
+				{/*<QuizPage title={'Test Quiz Page'} questions={AUTHENTICATION_QUIZ.questions} />*/}
 			</div>
 		</BaseTemplate>
 	);
