72637dcc056ed7e2d84763f98122b797f150ad20
brandonwong3 <bwong3@uw.edu>
2023-04-08 17:26:39 -0700

    Basically done for the card flip animation. Please run npm install to update the package dependencies first.

--- src/components/pages/TestPage.js
+++ src/components/pages/TestPage.js
@@ -1,18 +1,13 @@
 import React from "react";
 import BaseTemplate from '../templates/BaseTemplate';
-import QuizPage from './QuizPage';
-import AUTHENTICATION_QUIZ from '../../quizzes/authenticationQuiz';
-import FlipOverCard from '../fragments/intermediate/FlipOverCard';
+import FlipOverCardHolder from '../fragments/intermediate/FlipOverCardHolder';
 
 
 const TestPage = () => {
 	return (
 		<BaseTemplate>
 			<div className={'force-absolute-center'}>
-				<FlipOverCard question={'What is the difference between a cookie and a session?'}>
-					<p className={'text-center'}>A cookie is stored on the client's computer and is sent to the server with every request. A session is stored on the server and is identified by a session ID stored in a cookie.</p>
-				</FlipOverCard>
-				{/*<QuizPage title={'Test Quiz Page'} questions={AUTHENTICATION_QUIZ.questions} />*/}
+				<FlipOverCardHolder />
 			</div>
 		</BaseTemplate>
 	);
