b0504543140afa45595582af45f64705977783a3
brandonwong3 <bwong3@uw.edu>
2023-04-13 19:03:24 -0700

    Added the FlipOverCardHolder to each of the Reading Pages.

--- src/components/pages/stacks/practice/ReadingCrossSiteScriptingPage.js
+++ src/components/pages/stacks/practice/ReadingCrossSiteScriptingPage.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import BaseTemplate from '../../../templates/BaseTemplate';
+import FlipOverCardHolder from '../../../fragments/intermediate/FlipOverCardHolder';
+
+
+const LESSON_DATA = [
+	{
+		question: 'What is Reflected Cross-Site Scripting?',
+		answer: 'Reflected cross-site scripting can occur when malicious data in an HTTP request is reflected off of a web application to the victim\'s browser in an unsafe way. In other words, a malicious link is sent to a victim, and when the victim\'s browser loads or if the victim clicks on something loaded by the web application, an XSS request is sent and reflected back to the victim. Reflected XSS occurs when user input and requests are not sanitized properly and thoroughly so that an attacker can modify and manipulate how the web application functions and activate malicious code and scripts. Reflected XSS is dangerous because an attacker can view and modify information the user can perform, perform actions that the user can perform, and can potentially initiate potential malicious attacks by spoofing as the victim user. Moreover, an attacker can prompt the user to download malicious content, steal credentials/cookie values/sensitive data, and redirect the user to another website, all of which breach basic security principles of confidentiality, integrity, and availability, a giant threat to the web application and the security of the users.'
+	},
+	{
+		question: 'Risk Analysis',
+		answer: 'The risk associated with cross-site scripting varies widely based on the information stored on the website. If the website stores sensitive personal information, there is a greater risk with this vulnerability being present. If a malicious user is able to elevate its privileges, the risk level increases to a critical vulnerability with drastic impacts to the application and data stored in it.'
+	},
+	{
+		question: 'How to Prevent Reflected Cross-Site Scripting',
+		answer: 'As a general, never insert untrusted data anywhere, but if you must, HTML escape the data before putting it into the HTML element content. Moreover, encode data on output and use appropriate HTTP response headers so that browsers interpret data as text and not code.'
+	}
+]
+
 
 const ReadingCrossSiteScriptingPage = () => {
 	return (
@@ -10,19 +28,7 @@ const ReadingCrossSiteScriptingPage = () => {
 				<h1>Cross-Site Scripting</h1>
 			</div>
 			</div>
-			<div className="cross-site-scripting-desc">
-				<h2>What is Reflected Cross-Site Scripting?</h2>
-				<p> Reflected cross-site scripting can occur when malicious data in an HTTP request is reflected off of a web application to the victim's browser in an unsafe way. In other words, a malicious link is sent to a victim, and when the victim's browser loads or if the victim clicks on something loaded by the web application, an XSS request is sent and reflected back to the victim. Reflected XSS occurs when user input and requests are not sanitized properly and thoroughly so that an attacker can modify and manipulate how the web application functions and activate malicious code and scripts. Reflected XSS is dangerous because an attacker can view and modify information the user can perform, perform actions that the user can perform, and can potentially initiate potential malicious attacks by spoofing as the victim user. Moreover, an attacker can prompt the user to download malicious content, steal credentials/cookie values/sensitive data, and redirect the user to another website, all of which breach basic security principles of confidentiality, integrity, and availability, a giant threat to the web application and the security of the users. </p>
-			</div>
-			<div className="risk-desc">
-				<h2>Risk Analysis:</h2>
-				<p>The risk associated with cross-site scripting varies widely based on the information stored on the website. If the website stores sensitive personal information, there is a greater risk with this vulnerability being present. If a malicious user is able to elevate its privileges, the risk level increases to a critical vulnerability with drastic impacts to the application and data stored in it.</p>
-			</div>
-			<div className="prevention-desc">
-				<h2>How to Prevent Reflected Cross-Site Scripting:</h2>
-				<p> As a general, never insert untrusted data anywhere, but if you must, HTML escape the data before putting it into the HTML element content. Moreover, encode data on output and use appropriate HTTP response headers so that browsers interpret data as text and not code.
-				</p>
-			</div>
+			<FlipOverCardHolder data={LESSON_DATA}/>
 			<div className="btn-container">
  				<Link to= "/practice/cross-site-scripting">
 					 <button>See an Example!</button>
--- src/components/pages/stacks/practice/ReadingSQLInjectionPage.js
+++ src/components/pages/stacks/practice/ReadingSQLInjectionPage.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import BaseTemplate from '../../../templates/BaseTemplate';
+import FlipOverCardHolder from '../../../fragments/intermediate/FlipOverCardHolder';
+
+
+const LESSON_DATA = [
+	{
+		question: 'What is SQL Injection?',
+		answer: '<p>SQL injection is a vulnerability that can allow an attacker to interfere with the queries an application makes to the database and allow an attacker to view data that aren\'t authorized or traditionally able to view/retrieve. This is done by placing malicious SQL queries in the web page input. There is also the potential for an attacker to modify or delete the data, which can change the behavior or content of the application. Moreover, this vulnerability has the potential to allow an attacker to perform a denial of service attack or compromise the underlying server. SQL injection is detrimental to a business as an attacker can view sensitive data that may belong to other users of the application or any data being stored, such as passwords and credit card information. SQL injection has the potential to destroy your database and execute admin operations on the database.</p>'
+	},
+	{
+		question: 'Risk Analysis',
+		answer: '<p>SQL injection is a severe vulnerability because of the damage it can do. It can result in sensitive data exposure such as passwords, credit card details, and personal information, and alter the data in the database or the entire database itself. An attacker can drop tables, add new data, impersonate as the database administrator, or even access the operating system and attack the internal network. Due to the extent of what can be stolen with SQL injection, it is considered a severe vulnerability.</p>'
+	},
+	{
+		question: 'How to Prevent Against SQL Injection:',
+		answer: '<p>To remediate against SQL injection, it is important to use parameterized SQL queries and prepared statements so that way the database can distinguish what is code and what is data, regardless of the user input. With prepared statements, it ensures that an attacker cannot change the intent of a query, even if SQL commands are inserted. </p>',
+	},
+];
+
 
 const ReadingSQLInjectionPage = () => {
 	return (
@@ -10,18 +28,9 @@ const ReadingSQLInjectionPage = () => {
 				<h1>SQL Injection</h1>
 			</div>
 			</div>
-			<div className="SQL-injection-desc">
-				<h2>What is SQL Injection?</h2>
-				<p> SQL injection is a vulnerability that can allow an attacker to interfere with the queries an application makes to the database and allow an attacker to view data that aren't authorized or traditionally able to view/retrieve. This is done by placing malicious SQL queries in the web page input. There is also the potential for an attacker to modify or delete the data, which can change the behavior or content of the application. Moreover, this vulnerability has the potential to allow an attacker to perform a denial of service attack or compromise the underlying server. SQL injection is detrimental to a business as an attacker can view sensitive data that may belong to other users of the application or any data being stored, such as passwords and credit card information. SQL injection has the potential to destroy your database and execute admin operations on the database.</p>
-			</div>
-			<div className="risk-desc">
-				<h2>Risk Analysis:</h2>
-				<p>SQL injection is a severe vulnerability because of the damage it can do. It can result in sensitive data exposure such as passwords, credit card details, and personal information, and alter the data in the database or the entire database itself. An attacker can drop tables, add new data, impersonate as the database administrator, or even access the operating system and attack the internal network. Due to the extent of what can be stolen with SQL injection, it is considered a severe vulnerability.</p>
-			</div>
-			<div className="prevention-desc">
-				<h2>How to Prevent Against SQL Injection:</h2>
-				<p> To remediate against SQL injection, it is important to use parameterized SQL queries and prepared statements so that way the database can distinguish what is code and what is data, regardless of the user input. With prepared statements, it ensures that an attacker cannot change the intent of a query, even if SQL commands are inserted. </p>
-			</div>
+
+			<FlipOverCardHolder data={LESSON_DATA}/>
+
 			<div className="btn-container">
  				<Link to= "/practice/sql-injection">
 					 <button>See an Example!</button>
